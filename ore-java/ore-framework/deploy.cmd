5b3d9c4c93d267aee982322666679a68b4c72d38
huangzongzhi <huangzongzhi@yunovo.cn>
2019-12-31 18:38:31 +0800

    add

--- ore-java/ore-framework/deploy.cmd
+++
@@ -1,5 +0,0 @@
-REM Build Ore Framework script
-@echo off
-REM mvn clean deploy -Dmaven.test.skip=true  -DaltDeploymentRepository=nexus-releases::default::http://maven.yunovo.yunovo.cn:8081/nexus/content/repositories/thirdparty/
-
-mvn clean deploy -Dmaven.test.skip=true  -DaltDeploymentRepository=nexus-snapshots::default::http://maven.yunovo.yunovo.cn:8081/nexus/content/repositories/snapshots/
--- ore-java/ore-framework/install.cmd
+++
@@ -1,3 +0,0 @@
-@REM Build Ore Framework script
-@echo off
-mvn clean install -Dmaven.test.skip=true
