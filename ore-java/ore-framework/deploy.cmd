7bac3acedcf94519f43de1b79b074093c104ab13
huangzongzhi <huangzongzhi@yunovo.cn>
2020-01-13 15:49:09 +0800

    update pom.xml

--- ore-java/ore-framework/deploy.cmd
+++
@@ -1,5 +0,0 @@
-REM Build Ore Framework script
-@echo off
-mvn clean deploy -Dmaven.test.skip=true  -DaltDeploymentRepository=nexus-releases::default::http://maven.yunovo.yunovo.cn:8081/nexus/content/repositories/thirdparty/
-
-REM mvn clean deploy -Dmaven.test.skip=true  -DaltDeploymentRepository=nexus-snapshots::default::http://maven.yunovo.yunovo.cn:8081/nexus/content/repositories/snapshots/
